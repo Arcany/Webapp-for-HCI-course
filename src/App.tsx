d1e38f01b027b3485cf5c89dc246f23a1c8864fc
Märt Mäemees <--local>
2020-04-28 17:03:12 +0300

    Add react-router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import ShopView from './components/ShopView';
+import Checkout from './components/Checkout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">
+                Main View
+              </Link>
+            </li>
+            <li>
+              <Link to="/checkout">
+                Checkout
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <ShopView />
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ShopView.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ShopView extends React.Component {
+  render() {
+    return (
+      <div className="shopview">
+        Products go here.
+      </div>
+    );
+  }
+}
+
+export default ShopView;
