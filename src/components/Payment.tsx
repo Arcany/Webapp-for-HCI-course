e1c29496e2e20a8e8c07350c255f5a527227529a
Maria <mariamurumaaa@gmail.com>
2020-05-08 10:52:43 +0300

    Shipping ang payment finished

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {ReduxProps} from '../containers/PaymentContainer';
-import {Button, Col, Form, Row} from "react-bootstrap";
+import {Button, Col, Form} from "react-bootstrap";
 
 
 class Payment extends React.Component<ReduxProps, {}> {
@@ -8,56 +8,65 @@ class Payment extends React.Component<ReduxProps, {}> {
         return (
             <div>
                 <h1>Payment information:</h1>
+                <div id="andmed">
+                    <Form>
+                        <Form.Group controlId="formGridPaymentType">
+                            <Form.Label>Payment type</Form.Label>
+                            <Form.Control as="select" custom>
+                                <option>Credit Card</option>
+                                <option disabled>Bank Transfer -> Not available yet</option>
+                                <option disabled> Cash On Pickup -> Not available yet</option>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridCardType">
+                                <Form.Label>Card type</Form.Label>
+                                <Form.Control as="select" custom>
+                                    <option>Visa</option>
+                                    <option>MasterCard</option>
+                                    <option> Maestro</option>
+                                    <option> American Express</option>
+                                </Form.Control>
+                            </Form.Group>
 
-                <Form className="text-center">
-                    <Form.Group controlId="formNumber">
-                        <Form.Label>CARD NUMBER</Form.Label>
-                        <Col md={{span: 4, offset: 4}}> <Form.Control type="digits" placeholder="1111 2222 3333 9999"/></Col>
-                    </Form.Group>
+                            <Form.Group as={Col} controlId="formGridExprDate">
+                                <Form.Label>Expiration Date </Form.Label>
+                                <Form.Control type="text" placeholder="12/20"/>
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridCardNR">
+                                <Form.Label>Card Number</Form.Label>
+                                <Form.Control type="text" placeholder="1111 2222 3333 9999"/>
+                            </Form.Group>
 
+                            <Form.Group as={Col} controlId="formGridCVV">
+                                <Form.Label>CVV </Form.Label>
+                                <Form.Control type="password" placeholder="367"/>
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Group controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Use delivery address as card holder address"/>
+                        </Form.Group>
+                        <Form.Text className="text-muted">
+                            We'll never share your personal information with anyone else.
+                        </Form.Text>
+                        <div id="nupud">
 
-                    <Form.Group controlId="formName">
-                        <Form.Label>CARDHOLDERS NAME</Form.Label>
-                        <Col sm={5} md={{span: 4, offset: 4}}> <Form.Control type="name" placeholder="Alex Bee"/></Col>
-                    </Form.Group>
-                    <div id="keskele">
-                        <Row>
-                            <div id="ex">
-                                <Form.Group controlId="formDate">
-                                    <Form.Label>EXPIRATION DATE</Form.Label>
-                                    <Col><Form.Control type="text" placeholder="12/20"/></Col>
-                                </Form.Group>
-                            </div>
-                            <div id="cvv">
-                                <Form.Group controlId="formCVV">
-                                    <Form.Label>CVV</Form.Label>
-                                    <Col><Form.Control type="text" placeholder="000"/></Col>
-                                </Form.Group>
-                            </div>
-                        </Row>
-                    </div>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="I agree with the terms and conditions"/>
-                    </Form.Group>
-                    <Form.Text className="text-muted">
-                        We'll never share your personal information with anyone else.
-                    </Form.Text>
-                    <Row>
-                        <Col>
                             <Button variant="primary" type="submit" href="/shipping">
-                                BACK
+                                BACK TO DELIVERY
                             </Button>
-                        </Col>
-                        <Col>
+
                             <Button variant="primary" type="submit">
-                                CONFIRM
+                                SUBMIT PAYMENT
                             </Button>
-                        </Col>
-                    </Row>
-                </Form>
+                        </div>
+                    </Form>
+                </div>
             </div>
         );
     }
+
 }
 
 export default Payment;
--- src/components/Shipping.tsx
+++ src/components/Shipping.tsx
@@ -1,16 +1,103 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { ReduxProps } from '../containers/ShippingContainer';
+import {ReduxProps} from '../containers/ShippingContainer';
+import {Button, Col, Form} from "react-bootstrap";
 
 class Shipping extends React.Component<ReduxProps, {}> {
-  render() {
-    return (
-      <div>
-        <Link to="/checkout">Back</Link>
-        <Link to="/payment">Payment</Link>
-      </div>
-    );
-  }
+    render() {
+        return (<div>
+                <h1>Shipping information</h1>
+                <div id="andmed">
+                    <Form>
+                        <Form.Group controlId="formGridCounty">
+                            <Form.Label>Delivery method</Form.Label>
+                            <Form.Control as="select" custom>
+                                <option>Scheduled delivery</option>
+                                <option disabled>Parcel locker -> Not available yet</option>
+                                <option disabled> Pickup at the store -> Not available yet</option>
+                            </Form.Control>
+
+                        </Form.Group>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridFname">
+                                <Form.Label>FIRST NAME</Form.Label>
+                                <Form.Control type="text" placeholder="John"/>
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridLname">
+                                <Form.Label>LAST NAME</Form.Label>
+                                <Form.Control type="text" placeholder="Bee"/>
+                            </Form.Group>
+                        </Form.Row>
+
+                        <Form.Group controlId="formGridAddress1">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control placeholder="1234 Main St"/>
+                        </Form.Group>
+
+                        <Form.Group controlId="formGridAddress2">
+
+                            <Form.Control placeholder="Apartment, studio, or floor"/>
+                        </Form.Group>
+
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridCity">
+                                <Form.Label>City</Form.Label>
+                                <Form.Control/>
+                            </Form.Group>
+                            <Form.Group controlId="formGridCounty">
+                                <Form.Label>County</Form.Label>
+                                <Form.Control as="select" custom>
+                                    <option>Choose...</option>
+                                    <option>Harju County</option>
+                                    <option>Tartu County</option>
+                                    <option>Ida-Viru County</option>
+                                    <option>Pärnu County</option>
+                                    <option>Lääme-Viru County</option>
+                                    <option>Viljandi County</option>
+                                    <option>Rapla County</option>
+                                    <option>Võru County</option>
+                                    <option>SaareCounty</option>
+                                    <option>Jõgeva County</option>
+                                    <option>Järva County</option>
+                                    <option>Valga County</option>
+                                    <option>Põlva County</option>
+                                    <option>Lääne County</option>
+                                    <option>Hiiu County</option>
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridZip">
+                                <Form.Label>Zip</Form.Label>
+                                <Form.Control/>
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Group controlId="formGridAddress1">
+                            <Form.Label>Phone number</Form.Label>
+                            <Form.Control placeholder="+372 5584930"/>
+                        </Form.Group>
+                        <Form.Group controlId="formGridAddress1">
+                            <Form.Label>Notes to driver</Form.Label>
+                            <Form.Control as="textarea" placeholder="Hi! ...."/>
+                        </Form.Group>
+                        <div id="nupud2">
+                            <Form.Row>
+                                <Form.Group as={Col}>
+                                    <Button variant="primary" type="submit" href="/checkout">
+                                        BACK TO CHECKOUT
+                                    </Button>
+                                </Form.Group>
+
+                                <Form.Group as={Col}>
+                                    <Button variant="primary" type="submit" href="/payment">
+                                        SUBMIT SHIPPING</Button>
+                                </Form.Group>
+                            </Form.Row>
+                        </div>
+                    </Form>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Shipping;
