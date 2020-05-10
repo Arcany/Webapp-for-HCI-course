fd9c86b10ad5bae1e17de236d25e8886486ab4e8
Maria <mariamurumaaa@gmail.com>
2020-05-10 20:40:36 +0300

    pop up text

--- src/components/PaymentModal.tsx
+++ src/components/PaymentModal.tsx
@@ -16,7 +16,7 @@ class PaymentModal extends React.Component<ModalProps, {}> {
 
     return (
       <Modal show={this.props.show} onHide={handleClose} animation={false}>
-        <Modal.Body>Your order has been accepted, press "Yes" to be redirected to the home page.</Modal.Body>
+        <Modal.Body>Your order has been accepted. You will be notified when the courier is on their way!  Press "Yes" to be redirected to the home page.</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={() => handleClose(true)}>
 						Yes
